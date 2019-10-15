bbfa5b36357bccfc0855e9799a92df0ca398632a
Danny van der Meulen <dmeulen@ebay.com>
2019-10-15 13:28:13 +0200

    hm

--- jenkins_scripts/test.sh
+++ jenkins_scripts/test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -x
 #docker run -v ${WORKSPACE}:/config/ homeassistant/home-assistant:0.99.3 python -m homeassistant --script check_config --config /config
-docker run -v jenkins_home:/config/ homeassistant/home-assistant:0.99.3 ls /config
+docker run -v jenkins_home:/jenkins/ homeassistant/home-assistant:0.99.3 ls /jenins/jobs/deploy_home-assistant/workspace
