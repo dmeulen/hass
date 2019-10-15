b84fe9e57b36309d6b4fcebaac98c95cb2568ea9
Danny van der Meulen <dmeulen@ebay.com>
2019-10-15 12:52:22 +0200

    use docker to run test

--- jenkins_scripts/test.sh
+++ jenkins_scripts/test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-${WORKSPACE}/hass/bin/hass -c ${WORKSPACE} --script check_config
+docker run -v ${WORKSPACE}:/config homeassistant/home-assistant:0.99.3 python -m homeassistant --script check_config --config /config
