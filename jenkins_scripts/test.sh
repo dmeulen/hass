69dd44177e79d7830ea8e1a974fb6e4b6d69ff34
Danny van der Meulen <dmeulen@ebay.com>
2019-10-15 13:30:35 +0200

    fugly fix

--- jenkins_scripts/test.sh
+++ jenkins_scripts/test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash -x
-#docker run -v ${WORKSPACE}:/config/ homeassistant/home-assistant:0.99.3 python -m homeassistant --script check_config --config /config
-docker run -v jenkins_home:/jenkins/ homeassistant/home-assistant:0.99.3 ls /jenkins/jobs/deploy_home-assistant/workspace
+# I know, fugly. Need to fix this
+docker run -v jenkins_home:/jenkins/ homeassistant/home-assistant:0.99.3 python -m homeassistant --script check_config --config /jenkins/jobs/deploy_home-assistant/workspace
+
