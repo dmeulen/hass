520970396127949f52ee230e9c945d8b7daa71b9
Danny van der Meulen <dmeulen@ebay.com>
2019-10-15 13:07:00 +0200

    fix test

--- jenkins_scripts/test.sh
+++ jenkins_scripts/test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -x
-docker run -v ${WORKSPACE}:/config homeassistant/home-assistant:0.99.3 python -m homeassistant --script check_config
+docker run -v ${WORKSPACE}:/config/ homeassistant/home-assistant:0.99.3 python -m homeassistant --script check_config --config /config
